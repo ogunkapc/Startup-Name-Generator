ac96e51901aad82bba35b06599f71e327b6f6acd
unknown <ogunkapc@gmail.com>
2023-01-02 19:44:17 +0100

    Created stateful widget - RandomWords

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 // Copyright 2018 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -20,9 +18,24 @@ class MyApp extends StatelessWidget {
           title: const Text('Startup Name Generator'),
         ),
         body: const Center(
-          child: Text("A startup name"),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
